8360b0c131a6159a1a470a7a91d64c548066d6c4
Richie <ricsam@users.noreply.github.com>
2025-01-02 13:28:23 +0100

    add mkdir

--- fs/src/index.ts
+++ fs/src/index.ts
@@ -1,5 +1,5 @@
 export { AuthHandler } from "./auth-handler";
 export type { CredentialsFile } from "./auth-handler";
 export { EnstoreFs } from "./enstore-fs";
-export type { EnstoreFsOptions } from "./enstore-fs";
+export type { EnstoreFsOptions } from "./types";
 export { EnstorePromiseFs } from "./enstore-promise-fs";
